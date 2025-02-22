161c3b76f896d85270e963e2d2a0db87a1704fd3
reszkojr <s.reszkojr@gmail.com>
2025-02-22 13:53:55 -0300

    authentication service

---
+++ app/services/auth.tsx
@@ -0,0 +1,28 @@
+import { clearAuthTokens, setAccessToken, setAuthTokens } from "axios-jwt";
+import { api } from "~/api";
+
+interface LoginParams { 
+    email: string;
+    password: string;
+}
+
+interface RegisterParams {
+    email: string;
+    password: string;
+    username: string;
+}
+
+export const register = async (params: RegisterParams): Promise<void> => {
+    return await api.post('/api/register', params);
+}
+
+export const login = async (params: LoginParams): Promise<void> => {
+    const response = await api.post('/api/login', params);
+
+    setAuthTokens({
+		accessToken: response.data.accessToken,
+        refreshToken: response.data.refreshToken,
+	});
+}
+
+export const logout = async () => await clearAuthTokens();
