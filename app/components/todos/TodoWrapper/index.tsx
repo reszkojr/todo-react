d3ee88c42bd52eac9207b771ad4b33f1da66093e
Fabio S. Reszko Jr. <s.reszkojr@gmail.com>
2025-02-24 13:50:13 -0300

    move todo.context to own context folder

--- app/components/todos/TodoWrapper/index.tsx
+++ app/components/todos/TodoWrapper/index.tsx
@@ -1,10 +1,10 @@
 import { Outlet } from 'react-router';
-import { TodoProvider } from '~/contexts/todo.context';
+import { TodoProvider } from '~/contexts/todo/todo.context';
 
 export default function TodoWrapper() {
-	return (
-		<TodoProvider>
-			<Outlet />
-		</TodoProvider>
-	);
+    return (
+        <TodoProvider>
+            <Outlet />
+        </TodoProvider>
+    );
 }
--- app/routes/todos/todos.tsx
+++ app/routes/todos/todos.tsx
@@ -1,59 +1,59 @@
-import React, { use, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { DndContext, type DragEndEvent } from '@dnd-kit/core';
 import type Todo from '~/types/Todo';
 import DroppableColumn from '~/components/todos/DraggableColumn';
 import TodoItem from '~/components/todos/TodoItem';
-import { useTodo } from '~/contexts/todo.context';
+import { useTodo } from '~/contexts/todo/todo.context';
 
 const statuses = {
-	pending: 'Pending',
-	'in progress': 'In Progress',
-	completed: 'Completed',
+    pending: 'Pending',
+    'in progress': 'In Progress',
+    completed: 'Completed',
 };
 
 const TodosPage = () => {
-	const { todos, updateTodoStatus } = useTodo();
-	const [todosElements, setTodosElements] = useState<Todo[]>(todos);
+    const { todos, updateTodoStatus } = useTodo();
+    const [todosElements, setTodosElements] = useState<Todo[]>(todos);
 
-	useEffect(() => {
-		setTodosElements(todos);
-	}, [todos]);
+    useEffect(() => {
+        setTodosElements(todos);
+    }, [todos]);
 
-	const updateItems = (event: DragEndEvent) => {
-		const { active, over } = event;
+    const updateItems = (event: DragEndEvent) => {
+        const { active, over } = event;
 
-		if (over) {
-			const activeTodo = todos.find((todo) => todo.id.toString() === active.id);
-			const overStatus = over.id;
+        if (over) {
+            const activeTodo = todos.find((todo) => todo.id.toString() === active.id);
+            const overStatus = over.id;
 
-			if (activeTodo) {
-				setTodosElements((prevTodos) => prevTodos.map((todo) => (todo.id === activeTodo.id ? { ...todo, status: overStatus } : todo)) as Array<Todo>);
-				updateTodoStatus(activeTodo, overStatus as 'pending' | 'in progress' | 'completed');
-			}
-		}
-	};
+            if (activeTodo) {
+                setTodosElements((prevTodos) => prevTodos.map((todo) => (todo.id === activeTodo.id ? { ...todo, status: overStatus } : todo)) as Array<Todo>);
+                updateTodoStatus(activeTodo, overStatus as 'pending' | 'in progress' | 'completed');
+            }
+        }
+    };
 
-	return (
-		<>
-			<div className='flex p-3 mt-6'>
-				<h1 className='text-2xl font-bold'>To-do list</h1>
-                
-			</div>
-			<DndContext onDragEnd={updateItems}>
-				<div className='flex space-x-4 p-3'>
-					{Object.keys(statuses).map((status) => (
-						<DroppableColumn key={status} id={status} title={statuses[status as keyof typeof statuses]}>
-							{todosElements
-								.filter((todo) => todo.status === status)
-								.map((todo) => (
-									<TodoItem key={todo.id} todo={todo} />
-								))}
-						</DroppableColumn>
-					))}
-				</div>
-			</DndContext>
-		</>
-	);
+    return (
+        <>
+            <div className='flex p-3 mt-6'>
+                <h1 className='text-2xl font-bold'>To-do list</h1>
+
+            </div>
+            <DndContext onDragEnd={updateItems}>
+                <div className='flex space-x-4 p-3'>
+                    {Object.keys(statuses).map((status) => (
+                        <DroppableColumn key={status} id={status} title={statuses[status as keyof typeof statuses]}>
+                            {todosElements
+                                .filter((todo) => todo.status === status)
+                                .map((todo) => (
+                                    <TodoItem key={todo.id} todo={todo} />
+                                ))}
+                        </DroppableColumn>
+                    ))}
+                </div>
+            </DndContext>
+        </>
+    );
 };
 
 export default TodosPage;
