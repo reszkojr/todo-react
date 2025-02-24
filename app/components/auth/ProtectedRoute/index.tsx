f90fdbfc690993f4714afc0374cd7f4929be4799
Fabio S. Reszko Jr. <s.reszkojr@gmail.com>
2025-02-24 14:43:31 -0300

    use NODE_ENV from process.env

--- app/components/auth/ProtectedRoute/index.tsx
+++ app/components/auth/ProtectedRoute/index.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router'
+import { useLocation, useNavigate } from 'react-router'
 
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
     const navigator = useNavigate();
+
     let isAuthenticated;
 
-    useEffect(() => {
-        isAuthenticated = localStorage.getItem(`auth-tokens-${import.meta.env.NODE_ENV}`) || false;
+    useEffect(() => {        
+        isAuthenticated = localStorage.getItem(`auth-tokens-${process.env.NODE_ENV}`) || false;
         if (!isAuthenticated) {
             navigator('/auth/login')
         }
