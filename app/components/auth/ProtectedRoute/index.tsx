14bf10da077ebc9f4cf90579dce5f1c173b6fa4d
reszkojr <s.reszkojr@gmail.com>
2025-02-25 19:49:09 -0300

    make ProtectedRoute more efficient

--- app/components/auth/ProtectedRoute/index.tsx
+++ app/components/auth/ProtectedRoute/index.tsx
@@ -1,19 +1,22 @@
-import React, { useEffect } from 'react'
-import { Outlet, useLocation, useNavigate } from 'react-router'
+import React, { useEffect } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router';
 
 const ProtectedRoute = () => {
-    const navigator = useNavigate();
+	const navigator = useNavigate();
+	const location = useLocation();
 
-    let isAuthenticated;
+	let isAuthenticated;
 
-    useEffect(() => {        
-        isAuthenticated = localStorage.getItem(`auth-tokens-${process.env.NODE_ENV}`) || false;
-        if (!isAuthenticated) {
-            navigator('/auth/login')
-        }
-    }, []);
+	useEffect(() => {
+		isAuthenticated = localStorage.getItem(`auth-tokens-${process.env.NODE_ENV}`) || false;
+		if (!isAuthenticated) {
+			navigator('/auth/login');
+		}
+	}, [location]);
 
-    return <Outlet />
-}
+	if (isAuthenticated) {
+		return <Outlet />;
+	}
+};
 
 export default ProtectedRoute;
