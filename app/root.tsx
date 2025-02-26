6c7597b42b3ff06dc466379fdeeb4d4f94d2879e
Fabio S. Reszko Jr. <s.reszkojr@gmail.com>
2025-02-26 09:18:08 -0300

    move ToastContainer to its own div

--- app/root.tsx
+++ app/root.tsx
@@ -2,25 +2,27 @@ import { Links, Meta, Outlet, Scripts, ScrollRestoration } from 'react-router';
 import { Slide, ToastContainer } from 'react-toastify';
 
 export function Layout({ children }: { children: React.ReactNode }) {
-	return (
-		<html lang='en'>
-			<head>
-				<meta charSet='utf-8' />
-				<meta name='viewport' content='width=device-width, initial-scale=1' />
-				<Meta />
-				<Links />
-				<link rel='stylesheet' href='/app/app.css' />
-			</head>
-			<body>
-				{children}
-				<ScrollRestoration />
-				<Scripts />
-			</body>
-			<ToastContainer position='top-right' autoClose={3400} hideProgressBar newestOnTop closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme='dark' transition={Slide} />
-		</html>
-	);
+    return (
+        <html lang='en'>
+            <head>
+                <meta charSet='utf-8' />
+                <meta name='viewport' content='width=device-width, initial-scale=1' />
+                <Meta />
+                <Links />
+                <link rel='stylesheet' href='/app/app.css' />
+            </head>
+            <body>
+                {children}
+                <ScrollRestoration />
+                <Scripts />
+                <div>
+                    <ToastContainer position='top-right' autoClose={3400} hideProgressBar newestOnTop closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme='dark' transition={Slide} />
+                </div>
+            </body>
+        </html>
+    );
 }
 
 export default function App() {
-	return <Outlet />;
+    return <Outlet />;
 }
